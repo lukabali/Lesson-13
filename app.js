55010c36ebc89f0b8f342384b5e46f6a138be644
lukabali <lukabali24@gmail.com>
2022-06-20 18:18:03 +0400

    added js

---
+++ app.js
@@ -0,0 +1,16 @@
+const isNewYearToday = false;
+const string = 'string';
+const number = 5;
+const myUndefined = undefined;
+const n = null;
+
+
+const myName = "Luka";
+const myHobby = "basketball";
+const yearOfBirth = 2000;
+const currentYear = 2022;
+const myAge = currentYear - yearOfBirth;
+
+let text1 = "My name is " + myName + ", I am " + myAge + " years old and my hobby is " + myHobby;
+
+let text2 = `My name is ${myName}, I am ${currentYear - yearOfBirth} years old and my hobby is ${myHobby}`;
